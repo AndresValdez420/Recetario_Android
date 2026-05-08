6a6fb0a99be96ae2b9b3fef8a0f9564f4fd09e75
Ironicpanda45 <88987033+Ironicpanda45@users.noreply.github.com>
2026-05-08 12:09:45 -0600

    Update Recetario.kt

--- RecetarioBase/app/src/main/java/com/example/recetariobase/componentes/Recetario.kt
+++ RecetarioBase/app/src/main/java/com/example/recetariobase/componentes/Recetario.kt
@@ -1,12 +1,8 @@
 package com.example.recetariobase.componentes
 
 import androidx.compose.runtime.Composable
-import com.example.recetariobase.MainContent
-import com.example.recetariobase.ui.theme.RecetarioBaseTheme
 
 @Composable
-fun ColumnaTarjetas() {
-    RecetarioBaseTheme {
-        MainContent()
-    }
+fun PlatilloCard(nombre:String = "Platillo", tiempo: String = "-- min", porciones: String) {
+
 }
