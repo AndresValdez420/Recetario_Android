59285a05a7105343b246568404e71ba88156dfdd
Ironicpanda45 <88987033+Ironicpanda45@users.noreply.github.com>
2026-05-08 12:00:02 -0600

    Carpeta componentes

---
+++ RecetarioBase/app/src/main/java/com/example/recetariobase/componentes/Recetario.kt
@@ -0,0 +1,12 @@
+package com.example.recetariobase.componentes
+
+import androidx.compose.runtime.Composable
+import com.example.recetariobase.MainContent
+import com.example.recetariobase.ui.theme.RecetarioBaseTheme
+
+@Composable
+fun ColumnaTarjetas() {
+    RecetarioBaseTheme {
+        MainContent()
+    }
+}
